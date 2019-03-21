93c11f67478344c8f588cbb5bb0e5cb5e2c57f2a
DannythePastaMan <dgomez0998@gmail.com>
2019-03-21 12:29:51 -0600

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include "arbolB.h"
+#include "Archivo.h"
+
+
+
+using namespace std;
+
+int main(){
+
+
+
+    int val, opt, max;
+	int cant;
+    cout<<"LLaves por paginas: ";
+    cin>>max;
+	/*BTree * t = new BTree(max);
+*/
+	BTree * t = new BTree(max);
+
+	cout << "Cuantos registros quiere: ";
+	cin >>	cant;
+
+	t= Registro(cant,t,max);
+
+	
+
+    while (true) {
+        cout<<"[1] Insertar\n[2] Buscar\n";
+        cout<<"[3] Impresion (min a max)\n";
+        cout<<"[4] Salir\n Opcion: ";
+        cin >> opt;
+        cout << endl;
+        switch (opt) {
+            case 1:
+              /*  cout<<"Ingrese nodo:";
+                cin >> val;
+				t->insert(val,);*/
+
+                break;
+            case 2:
+                cout<<"Elemento a buscar:";
+                cin >> val;
+                (t->search(val) != NULL)? cout << "\nExiste" : cout << "\nNo Existe";
+                cout << endl;
+                break;
+            case 3:
+				t->traverse();
+                cout << endl;
+                break;
+            default:
+                exit(0);
+        }
+        cout << endl;
+    }
+}
